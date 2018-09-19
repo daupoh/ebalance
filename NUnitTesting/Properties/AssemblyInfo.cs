0505701a93fadf7d7999bd1da16ef37e2eedec47
Стоялов Евгений <levievgen26@gmail.com>
2018-09-19 10:16:04 +0300

    bullshit

---
+++ NUnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NUnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NUnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("11e4e5e0-cc52-41e7-8c29-7aaccf6dd25e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NUnitTests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NUnitTests
+{
+    public class Class1
+    {
+    }
+}
